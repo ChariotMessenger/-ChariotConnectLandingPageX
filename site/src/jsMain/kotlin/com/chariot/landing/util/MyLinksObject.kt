fd95e7138558ec2ab00294b1d9c322c3b74508f6
kizito18 <kizzy.ok99@gmail.com>
2026-09-09 19:17:53 +0100

    Commit now

--- site/src/jsMain/kotlin/com/chariot/landing/util/MyLinksObject.kt
+++ site/src/jsMain/kotlin/com/chariot/landing/util/MyLinksObject.kt
@@ -2,15 +2,12 @@ package com.chariot.landing.util
 
 object MyLinksObject {
 
-    const val customersAppLinkIosStore = "https://kizito-portfolio.zitoscode.com/"
-    const val ridersAppLinkIosStore = "https://kizito-portfolio.zitoscode.com/"
-    const val vendorsAppLinkIosStore = "https://kizito-portfolio.zitoscode.com/"
+    const val customersAppLinkIosStore = "https://apps.apple.com/app/id1234567890"
+    const val ridersAppLinkIosStore = "https://apps.apple.com/app/id2345678901"
+    const val vendorsAppLinkIosStore = "https://apps.apple.com/app/id3456789012"
 
-
-
-    const val customersAppLinkPlayStore = "https://kizito-portfolio.zitoscode.com/"
-    const val ridersAppLinkPlayStore = "https://kizito-portfolio.zitoscode.com/"
-    const val vendorsAppLinkPlayStore = "https://kizito-portfolio.zitoscode.com/"
-
+    const val customersAppLinkPlayStore = "https://play.google.com/store/apps/details?id=com.chariotconnect.customers"
+    const val ridersAppLinkPlayStore = "https://play.google.com/store/apps/details?id=com.chariotconnect.riders"
+    const val vendorsAppLinkPlayStore = "https://play.google.com/store/apps/details?id=com.chariotconnect.vendors"
 
 }
--- site/src/jsMain/kotlin/com/chariot/landing/util/ResObject.kt
+++ site/src/jsMain/kotlin/com/chariot/landing/util/ResObject.kt
@@ -71,7 +71,6 @@ object ResObject {
         const val happy_bottom6_img = "/happy_bottom1_img.png"
 
 
-        //v
 
     }
 
